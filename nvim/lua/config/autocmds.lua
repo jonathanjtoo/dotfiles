0214a403f679bdb8efbc9ba8b7dfba90a0a19161
Jonathan Too <jonathanjtoo@gmail.com>
2025-10-24 17:39:47 -0700

    [nvim] fix config formatting based on editorconfig

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -25,55 +25,55 @@ vim.api.nvim_create_autocmd({ "WinLeave", "FocusLost", "InsertEnter" }, {
 
 
 vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(event)
-		local buf_map = function(mode, rhs, lhs)
-			vim.keymap.set(mode, rhs, lhs, {buffer = event.buf})
-		end
+  callback = function(event)
+    local buf_map = function(mode, rhs, lhs)
+      vim.keymap.set(mode, rhs, lhs, {buffer = event.buf})
+    end
 
-		-- These keymaps are the defaults in Neovim v0.11
-		buf_map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-		buf_map('n', 'grr', '<cmd>lua vim.lsp.buf.references()<cr>')
-		buf_map('n', 'gri', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-		buf_map('n', 'grn', '<cmd>lua vim.lsp.buf.rename()<cr>')
-		buf_map('n', 'gra', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-		buf_map('n', 'gO', '<cmd>lua vim.lsp.buf.document_symbol()<cr>')
-		buf_map({'i', 's'}, '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+    -- These keymaps are the defaults in Neovim v0.11
+    buf_map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+    buf_map('n', 'grr', '<cmd>lua vim.lsp.buf.references()<cr>')
+    buf_map('n', 'gri', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+    buf_map('n', 'grn', '<cmd>lua vim.lsp.buf.rename()<cr>')
+    buf_map('n', 'gra', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    buf_map('n', 'gO', '<cmd>lua vim.lsp.buf.document_symbol()<cr>')
+    buf_map({'i', 's'}, '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
 
-		-- These are custom keymaps
-		-- Go to definition
-		buf_map({'n', 'v'}, 'gd', vim.lsp.buf.definition)
-		-- Peek definition (open definition in a floating window)
-		buf_map({'n', 'v'}, 'gD', vim.lsp.buf.declaration)
+    -- These are custom keymaps
+    -- Go to definition
+    buf_map({'n', 'v'}, 'gd', vim.lsp.buf.definition)
+    -- Peek definition (open definition in a floating window)
+    buf_map({'n', 'v'}, 'gD', vim.lsp.buf.declaration)
 
-		-- Hover documentation
-		buf_map('n', 'K', vim.lsp.buf.hover)
+    -- Hover documentation
+    buf_map('n', 'K', vim.lsp.buf.hover)
 
-		-- Show diagnostics
-		buf_map('n', '<leader>d', vim.diagnostic.open_float)
-		-- Go to next diagnostic message
-		buf_map('n', ']d', vim.diagnostic.goto_next)
+    -- Show diagnostics
+    buf_map('n', '<leader>d', vim.diagnostic.open_float)
+    -- Go to next diagnostic message
+    buf_map('n', ']d', vim.diagnostic.goto_next)
 
-		-- Go to previous diagnostic message
-		buf_map('n', '[d', vim.diagnostic.goto_prev)
+    -- Go to previous diagnostic message
+    buf_map('n', '[d', vim.diagnostic.goto_prev)
 
-		-- Rename symbol
-		buf_map('n', '<leader>rn', vim.lsp.buf.rename)
+    -- Rename symbol
+    buf_map('n', '<leader>rn', vim.lsp.buf.rename)
 
-		-- Code actions
-		buf_map('n', '<leader>ca', vim.lsp.buf.code_action)
-	end,
+    -- Code actions
+    buf_map('n', '<leader>ca', vim.lsp.buf.code_action)
+  end,
 })
 
 vim.api.nvim_create_autocmd({"InsertEnter"}, {
-    callback = function()
-        -- vim.opt_local.formatoptions:remove({"c", "r", "o"})
-	vim.opt_paste = true
-    end
+  callback = function()
+    -- vim.opt_local.formatoptions:remove({"c", "r", "o"})
+    vim.opt_paste = true
+  end
 })
 
 vim.api.nvim_create_autocmd({"InsertLeave"}, {
-    callback = function()
-        -- vim.opt_local.formatoptions:append({"c", "r", "o"})
-	vim.opt_paste = false
-    end
+  callback = function()
+    -- vim.opt_local.formatoptions:append({"c", "r", "o"})
+    vim.opt_paste = false
+  end
 })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -12,16 +12,16 @@ vim.opt.softtabstop = 4
 vim.opt.scrolloff = 10
 
 if not vim.g.vscode then
-	vim.opt.clipboard:append "unnamedplus"
-	vim.g.clipboard = {
-		name = 'OSC 52',
-		copy = {
-			['+'] = require('vim.ui.clipboard.osc52').copy '+',
-			['*'] = require('vim.ui.clipboard.osc52').copy '*',
-		},
-		paste = {
-			['+'] = require('vim.ui.clipboard.osc52').paste '+',
-			['*'] = require('vim.ui.clipboard.osc52').paste '*',
-		},
-	}
+  vim.opt.clipboard:append "unnamedplus"
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+      ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+      ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+    },
+  }
 end
