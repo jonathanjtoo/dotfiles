2e4bb59359fdea4e1012d7e2e5981e1a6983e317
Jonathan Too <jonathanjtoo@gmail.com>
2025-10-30 12:41:45 -0700

    [nvim] update clangd lsp to save .cache in project root

--- nvim/lsp/clangd.lua
+++ nvim/lsp/clangd.lua
@@ -1,4 +1,16 @@
 -- override the cmd in nvim-lspconfig to do --background-index
+local function find_root()
+  local path = vim.fs.find({ "compile_commands.json", ".git" }, { upward = true, type = "file" })[1]
+  return path and vim.fs.dirname(path) or vim.fn.getcwd()
+end
+
 vim.lsp.config('clangd', {
-  cmd = { 'clangd', '--background-index' },
+  cmd = {
+    'clangd',
+    '--background-index',
+    "--compile-commands-dir=" .. find_root(),
+    "--pch-storage=memory",
+  },
+  root_dir = find_root(),
+  capabilities = require('cmp_nvim_lsp').default_capabilities()
 })
